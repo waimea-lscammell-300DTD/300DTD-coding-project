62c4ab96ce69ed5ba879d00a8b58fb1883829836
waimea-lscammell-300DTD <lscammell@waimea.school.nz>
2024-10-26 15:19:08 +1300

    update

--- src/Main.kt
+++ src/Main.kt
@@ -97,10 +97,10 @@ class GUI : JFrame(), ActionListener {
         scenes.add(chicken)
 
         // Level 1
-        val yes =  Scene("yes","yes", "src/images/medic.png")
-        val no =  Scene("no","no", "src/images/ubersaw medic.png")
-        val goaway =  Scene("go away","go away", "src/images/medic.png")
-        val share =  Scene("share","I've allready got some would you like to share?", "src/images/ubersaw medic.png")
+        val yes =  Scene("yes","yes", "src/images/scout happy.png")
+        val no =  Scene("no","no", "src/images/scout sad.png")
+        val goaway =  Scene("go away","go away", "src/images/Scout suprised.png")
+        val share =  Scene("share","Really you would want to share with me?", "src/images/scout chicken costume.png")
         scenes.add(yes)
         scenes.add(no)
         scenes.add(goaway)
@@ -108,13 +108,13 @@ class GUI : JFrame(), ActionListener {
         chicken.addConnection(1, "yes", yes)
         chicken.addConnection(2, "no", no)
         chicken.addConnection(3, "go away", goaway)
-        chicken.addConnection(4, "smile", share)
+        chicken.addConnection(4, "share", share)
 
 
         // Yes
-        val eat =  Scene("eat ","eating chicken*", "src/images/medic.png")
-        val take =  Scene("take ","takes chicken*", "src/images/ubersaw medic.png")
-        val cook =  Scene("cook","cookes the chicken some more*", "src/images/medic.png")
+        val eat =  Scene("eat ","eating chicken*", "src/images/scout chicken costume.png")
+        val take =  Scene("take ","takes chicken*", "src/images/Scout suprised.png")
+        val cook =  Scene("cook","cooks the chicken some more*", "src/images/chicken hat.png")
         scenes.add(eat)
         scenes.add(take)
         scenes.add(cook)
@@ -123,22 +123,42 @@ class GUI : JFrame(), ActionListener {
         yes.addConnection(3, "cook", cook)
 
         // No
-        val deny   =  Scene("deny ","ok ill just eat them myself :(", "src/images/medic.png")
-        scenes.add(eat)
+        val deny   =  Scene("deny ","ok ill just eat them myself :(", "src/images/scout sad.png")
+        scenes.add(deny)
         no.addConnection(1, "deny", deny)
 
+        //Go away
+        val deny2  = Scene("deny ","ok ill just eat them myself :(", "src/images/Scout cry.png")
+        scenes.add(deny2)
+        goaway.addConnection(1, "deny", deny2)
+
         // Cook
-        val finishcooking  =  Scene("Finish Cooking","You cook the chicken and it looks even tastier", "src/images/medic.png")
+        val finishcooking  =  Scene("Finish Cooking","You cook the chicken and it looks even tastier", "src/images/chicken hat.png")
+        scenes.add(finishcooking)
         cook.addConnection(3, "finish cooking", finishcooking)
         finishcooking.addConnection(1, "Finish cooking", yes)
 
+        //Share
+        val yeah  =  Scene("Finish Cooking","'yay' scout takes chicken*", "src/images/chicken hat.png")
+        val Iwasjoking  =  Scene("I was Joking","Why would you lie to me ;(", "src/images/Scout cry.png")
+
+        scenes.add(yeah)
+        scenes.add(Iwasjoking)
+        share.addConnection(1, "yeah", yeah)
+        share.addConnection(2, "I was Joking", Iwasjoking)
+
+
 
         // Things that bring you back to the start
-        val denial = Scene("denial","Scout walks away sadly", "src/images/medic.png")
-        val death = Scene("You died!", "scout didnt like you taking his chicken", "src/images/death.png")
+        val denial = Scene("denial","Scout walks away sadly", "src/images/scout sad.png")
+        val death = Scene("You died!", "scout didn't like you taking his chicken", "src/images/scout with gun.png")
+        val death2 = Scene("You died!", "scout doesn't like liars", "src/images/scout with gun.png")
         take.addConnection(1, "Take it", death)
+        Iwasjoking.addConnection(1, "I was joking", death2)
         deny.addConnection(1, "Scout leaves", denial)
+        deny2.addConnection(1, "Scout leaves", denial)
         death.addConnection(1, "Restart", chicken)
+        death2.addConnection(1, "Restart", chicken)
         denial.addConnection(1, "Restart", chicken)
     }
 
@@ -147,7 +167,7 @@ class GUI : JFrame(), ActionListener {
      */
     private fun setupWindow() {
         title = "Hello, World!"
-        contentPane.preferredSize = Dimension(1000, 500)
+        contentPane.preferredSize = Dimension(1500, 1000)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -162,36 +182,36 @@ class GUI : JFrame(), ActionListener {
         val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 20)
 
         descriptionLabel = JLabel("Scout appears and walks slowly up to you, he says 'do you have a bucket of chicken?'", SwingConstants.CENTER)
-        descriptionLabel.bounds = Rectangle(100, 10, 800, 40)
+        descriptionLabel.bounds = Rectangle(350, 50, 800, 40)
         descriptionLabel.font = baseFont
         add(descriptionLabel)
 
         choice1Button = JButton("Choice 1")
-        choice1Button.bounds = Rectangle(125,350,150,50)
+        choice1Button.bounds = Rectangle(150,650,250,50)
         choice1Button.font = baseFont
         choice1Button.addActionListener(this)
         add(choice1Button)
 
         choice2Button = JButton("Choice 2")
-        choice2Button.bounds = Rectangle(325,350,150,50)
+        choice2Button.bounds = Rectangle(450,650,250,50)
         choice2Button.font = baseFont
         choice2Button.addActionListener(this)
         add(choice2Button)
 
         choice3Button = JButton("Choice 3")
-        choice3Button.bounds = Rectangle(525,350,150,50)
+        choice3Button.bounds = Rectangle(800,650,250,50)
         choice3Button.font = baseFont
         choice3Button.addActionListener(this)
         add(choice3Button)
 
         choice4Button = JButton("Choice 4")
-        choice4Button.bounds = Rectangle(725,350,150,50)
+        choice4Button.bounds = Rectangle(1100,650,250,50)
         choice4Button.font = baseFont
         choice4Button.addActionListener(this)
         add(choice4Button)
 
         imageLabel = JLabel()
-        imageLabel.bounds = Rectangle(400, 50, 200, 200)
+        imageLabel.bounds = Rectangle(550, 150, 400, 400)
         add(imageLabel)
     }
 
@@ -242,7 +262,7 @@ class GUI : JFrame(), ActionListener {
         }
 
         var image = ImageIcon(currentScene.img).image
-        image = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH)
+        image = image.getScaledInstance(400, 400, Image.SCALE_SMOOTH)
         imageLabel.icon = ImageIcon(image)
     }
     /**
